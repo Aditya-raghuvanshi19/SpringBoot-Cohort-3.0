f440ab1d0af0c71e97e46850d204a69b678ed6fe
Aditya Raghuvanshi <raghuvanshiaditya999@gmail.com>
2025-11-17 23:46:39 +0530

    L-2.9 Lambda and stream API completed

--- Week 2/Lambda and stream API/Lambda.java
+++ Week 2/Lambda and stream API/Lambda.java
@@ -1,2 +1,51 @@
-package PACKAGE_NAME;public class Lambda {
+public class Lambda {
+    public static void main(String[] args) {
+
+        // 1st way -> by the first way
+//        Walkable obj = new WalkFast();
+//        obj.walk(5,true);
+
+        // 2nd way -> not the need of class implementing interface, on the go create class and override methods
+    Walkable obj2 = new Walkable(){
+        @Override
+        public int walk(int steps, boolean isEnabled) {
+            System.out.println("your steps are : "+ steps);
+            return 2*steps;
+        }
+    };
+
+    // 3rd way -> now we are using Lambda function to create an object of functionalInterface only
+        // and on the fly we define the method and create the object
+        // and we do not need to declare the datatype of the parameter of the function as their is only one function so compiler know that what are the sequence of the parameter , the name of the function and dataType
+        // arguments name should not be necessary to same but good practice is use same
+        // here now no need to write the interface on the write hand side
+        Walkable obj3 = (steps, isEnabled) -> {
+            System.out.println("Walking fast "+ steps +" steps.");
+            return 2*steps;
+        };
+        Walkable obj4 = (steps, isEnabled) -> 2*steps;
+        //to return directly from the Lambda function do not use {} just simple write that statement it will return.
+        System.out.println(obj4.walk(4, true));
+       System.out.println(obj3.walk(7,true));
+
+
+
+    }
+}
+
+//an interface having only single method is known as functionInterface and in this we can use the Lambda function to override and define the function
+// their is not need to mention @FunctionalInterface it is by default consider
+@FunctionalInterface
+interface Walkable{
+    int walk(int steps,boolean isEnabled);
+}
+// the 1st way we define a class which implement this interface and override the function
+
+class WalkFast implements Walkable{
+    @Override
+    public int walk(int steps,boolean isEnabled){
+        System.out.println("your steps are : "+ steps);
+        return 2*steps;
+
+    }
 }
--- Week 2/Lambda and stream API/StreamAPI.java
+++ Week 2/Lambda and stream API/StreamAPI.java
@@ -1,2 +1,51 @@
-package PACKAGE_NAME;public class StreamAPI {
+import java.util.List;
+import java.util.Map;
+import java.util.stream.Collectors;
+import java.util.stream.Stream;
+
+public class StreamAPI {
+
+    public static void main(String[] args) {
+
+
+        //once a stream is run then again we can not run the stream as stream runs and then it will be closed
+        //if we again want to run get error that stream is runs and closed
+        //Each time a stream is processed the previous stream is closed and a new stream will be created. To prevent memory leaking.
+        List<String> fruits = List.of("Apple","Banana","Mango","kiwi","orange");
+        //Stream<String> fruitsStream = fruits.stream();
+//        fruitsStream.forEach((fruit)-> {
+//            System.out.println(fruit);
+//        } );
+
+//        fruitsStream
+//                .filter((fruit) -> fruit.length() < 6)
+//                .sorted()
+//                .map(f->f.length())
+//                .map((fruitLength)-> 2*fruitLength)
+//                .forEach((fruit -> System.out.println(fruit)));
+
+//        List<Integer> fruitsLengthList = fruits.stream()
+//                .map((f)->f.length())
+//                .collect(Collectors.toList());
+        //the collect method uses collectors
+        //the collector is use to collect all those stream into a required collections it can be : List , Set , Map only
+
+//        System.out.println(fruitsLengthList);
+
+        Map<String , Integer> fruiteMap = fruits
+                .stream()
+                .collect(Collectors.toMap(
+                        key -> key,
+                        value -> value.length()
+                ));
+
+        System.out.println(fruiteMap);
+
+        // lambda method reference is used when we do not want to define the variable name in the lambda function we use  this so it is like -->    className :: methodName   so we have to remember of which class the method is then it will directly applied on the variable
+
+        // ex :  value -> value.length()  this lambda function can be written as  String::length
+
+        //  during lambda function we do not define the datatype of the argument just write the variable name
+
+    }
 }
